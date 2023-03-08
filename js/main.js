5bde152ae97b3de889578c94b81a1cf5975ebc28
Simone Carcagnì <simonecarcagni@libero.it>
2023-03-08 20:36:27 +0100

    Lavoro Terminato

--- js/main.js
+++ js/main.js
@@ -1,42 +1,18 @@
 containerDom = document.getElementById('container');
 
-
-
-let teamInformation = [
-    {
-        'name' : 'Wayne Barnett',
-        'role': 'Founder & CEO',
-        'img': 'wayne-barnett-founder-ceo.jpg'
-    },
-    {
-        'name': 'Angela Caroll',
-        'role': 'Chief Editor  ',
-        'img': 'angela-caroll-chief-editor.jpg'
-    },
-    {
-        'name': 'Walter Gordon',
-        'role': 'Office Manager',
-        'img': 'walter-gordon-office-manager.jpg'
-    },
-    {
-        'name': 'Angela Lopez',
-        'role': 'Social Media Manager',
-        'img': 'angela-lopez-social-media-manager.jpg'
-    },
-    {
-        'name': 'Scott Estrada',
-        'role': 'Developer',
-        'img': 'scott-estrada-developer.jpg'
-    },
-    {
-        'name': 'Barbara Ramos',
-        'role': 'Graphic Designer',
-        'img': 'barbara-ramos-graphic-designer.jpg'
-    },
-
-]
-
-for (let key in teamInformation){
-    console.log(teamInformation[key]);
+let teamInformation = {
+    name : ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"],
+    role: ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
+    img: ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"]
 }
+
+for (let i = 0; i < 6; i++){
+
+    containerDom.innerHTML += `<div class="card">
+                                ${teamInformation.name[i]}<br>
+                                ${teamInformation.role[i]}<br>
+                                <img src="./img/${teamInformation.img[i]}">`;
+    
+}
+
 
